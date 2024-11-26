769312ee5e8c0ff5d80c59f8ff5555983a3f5e71
Suryaa Essaki Muthu <e.suryaa@gmail.com>
2024-11-26 10:25:56 +0530

    changes for component navbar

--- src/components/DataEntryPage.jsx
+++ src/components/DataEntryPage.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import Navbar from "./components/NavBar";
-
+import Navbar from "./Navbar";
 const DataEntryPage = () => {
   const [formData, setFormData] = useState({
     product: "",
