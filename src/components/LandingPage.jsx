661f29852bbb1e9110da09c8b22e1aae1d68f3db
cvhkr19 <c.v.h.k.r19@gmail.com>
2024-11-27 21:24:43 +0530

    landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,27 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import "./LandingPage.css";
-
+import Spline from '@splinetool/react-spline';
 function LandingPage() {
   return (
     <div className="container">
-      <h1 className="header">Welcome to Supply Chain Management</h1>
-      <p className="description">
-        Streamline your shipment process and track analytics with ease.
-      </p>
-      <div className="button-container">
-        <Link to="/login">
-          <button className="button">Login</button>
-        </Link>
-        <Link to="/data-entry">
-          <button className="button">Enter Data</button>
-        </Link>
-        <Link to="/analytics">
-          <button className="button">View Analytics</button>
-        </Link>
-      </div>
+      <Spline scene="src\components\scene.splinecode" />
     </div>
   );
 }
 
 export default LandingPage;
+
+// import Spline from '@splinetool/react-spline';
+
+// export default function App() {
+//   return (
+//     <Spline scene="https://prod.spline.design/zjfHa1Sv1lzZFfKM/scene.splinecode" />
+//   );
+// }
